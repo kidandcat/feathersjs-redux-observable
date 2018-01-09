4dcffab74d46422c4ef2722b0b3e941abbec5eb0
Jairo <jairo.viciana@accenture.com>
2018-01-09 17:53:48 +0100

    using action in request

--- frontend/src/State/epics/server.epic.ts
+++ frontend/src/State/epics/server.epic.ts
@@ -5,15 +5,16 @@ import { Epic } from "redux-observable";
 import { ofType } from "redux-observable";
 import { switchMap, mapTo } from "rxjs/operators";
 import { Action } from "redux";
-import { ajax } from "rxjs/Observable/dom/ajax";
 import { fromPromise } from "rxjs/Observable/fromPromise";
 
 export const serverEpic: Epic<Action, {}> = action$ =>
   action$.pipe(
     ofType(ColorActions.changeColor),
-    switchMap((action: Action) =>
-      fromPromise(server.service("message").create({ text: "hello" })).pipe(
-        mapTo(colorSaved())
-      )
+    switchMap(action =>
+      fromPromise(
+        server
+          .service("message")
+          .create({ text: "hello", action: action.payload })
+      ).pipe(mapTo(colorSaved()))
     )
   );
